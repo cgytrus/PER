34b63451d594032f2a849d8c63caf5309b2f915b
ConfiG <cgytrus@cgyt.ru>
2023-06-21 13:55:07 +0300

    move dialog box screen to prr.ui

---
+++ PRR.UI/src/Resources/DialogBoxPaletteResource.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+
+using PER.Abstractions.Resources;
+using PER.Util;
+
+namespace PRR.UI.Resources;
+
+[PublicAPI]
+public class DialogBoxPaletteResource : Resource {
+    public const string GlobalId = "layouts/dialogBoxPalette";
+
+    protected override IEnumerable<KeyValuePair<string, string>> paths { get; } = new Dictionary<string, string> {
+        { "palette", "layouts/dialogBox.txt" }
+    };
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public string palette { get; private set; } = "                ";
+
+    public override void Load(string id) {
+        if(!TryGetPath("palette", out string? palettePath))
+            return;
+        string palette = File.ReadAllText(palettePath);
+        if(palette.Length < 16)
+            throw new InvalidDataException("Dialog box palette should be at least 16 characters long");
+        this.palette = palette;
+    }
+
+    public override void Unload(string id) { }
+
+    public char Get(int x, int y, Vector2Int size) =>
+        Get(x == 0, x == size.x - 1, y == 0, y == size.y - 1);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    // haha micro optimization go brrrr
+    public char Get(bool isStartX, bool isEndX, bool isStartY, bool isEndY) {
+        unsafe {
+            return BitConverter.IsLittleEndian ?
+                palette[*(byte*)&isStartX << 3 | *(byte*)&isEndX << 2 | *(byte*)&isStartY << 1 | *(byte*)&isEndY] :
+                palette[*(byte*)&isEndY << 3 | *(byte*)&isStartY << 2 | *(byte*)&isEndX << 1 | *(byte*)&isStartX];
+        }
+    }
+}
---
+++ PRR.UI/src/Screens/DialogBoxScreenResource.cs
@@ -0,0 +1,58 @@
+﻿using JetBrains.Annotations;
+
+using PER.Abstractions;
+using PER.Abstractions.Rendering;
+using PER.Abstractions.Resources;
+using PER.Abstractions.UI;
+using PER.Common.Resources;
+using PER.Util;
+
+using PRR.UI.Resources;
+
+namespace PRR.UI.Screens;
+
+[PublicAPI]
+public abstract class DialogBoxScreenResource : LayoutResource, IScreen {
+    protected abstract IResources resources { get; }
+
+    protected Vector2Int size { get; set; }
+
+    protected virtual string foregroundColorId => "dialogBox_fg";
+    protected virtual string backgroundColorId => "dialogBox_bg";
+    protected virtual IEffect? frameEffect => null;
+
+    private ColorsResource? _colors;
+    private DialogBoxPaletteResource? _palette;
+
+    protected DialogBoxScreenResource(Vector2Int size) => this.size = size;
+
+    public virtual void Open() {
+        if(!resources.TryGetResource(ColorsResource.GlobalId, out _colors) ||
+            !resources.TryGetResource(DialogBoxPaletteResource.GlobalId, out _palette))
+            throw new InvalidOperationException("Missing dependency.");
+    }
+
+    public virtual void Close() {
+        _colors = null;
+        _palette = null;
+    }
+
+    public virtual void Update(TimeSpan time) {
+        if(_colors is null || _palette is null ||
+            !_colors.colors.TryGetValue(backgroundColorId, out Color backgroundColor) ||
+            !_colors.colors.TryGetValue(foregroundColorId, out Color foregroundColor))
+            return;
+
+        Vector2Int offset = new((renderer.width - size.x) / 2, (renderer.height - size.y) / 2);
+        for(int y = 0; y < size.y; y++)
+            for(int x = 0; x < size.x; x++)
+                renderer.DrawCharacter(new Vector2Int(offset.x + x, offset.y + y),
+                    new RenderCharacter(_palette.Get(x, y, size), backgroundColor, foregroundColor),
+                    RenderOptions.Default, frameEffect);
+
+        foreach((string _, Element element) in elements)
+            element.Update(time);
+    }
+
+    public abstract void Tick(TimeSpan time);
+}
