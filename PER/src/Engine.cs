f75d1690c8c1a33b822f3d6dd3f651e259b034cc
ConfiG <cgytrus@cgyt.ru>
2023-06-26 00:50:17 +0300

    headless engine mode

--- PER/src/Engine.cs
+++ PER/src/Engine.cs
@@ -9,6 +9,7 @@ using PER.Abstractions.Audio;
 using PER.Abstractions.Input;
 using PER.Abstractions.Rendering;
 using PER.Abstractions.Resources;
+using PER.Headless;
 using PER.Util;
 
 namespace PER;
@@ -29,6 +30,8 @@ public class Engine {
     public IInput input { get; }
     public IAudio audio { get; }
 
+    private bool _headless;
+
     private readonly Stopwatch _clock = new();
     private TimeSpan _lastTickTime;
 
@@ -40,18 +43,42 @@ public class Engine {
         this.audio = audio;
     }
 
+    // runs the engine in headless mode
+    // intended for servers
+    public Engine(IResources resources, IGame game) {
+        _headless = true;
+        this.resources = resources;
+        this.game = game;
+        renderer = new HeadlessRenderer();
+        // crashing with nre is fine in headless mode
+        // games are supposed to implement the server in a different project
+        // and not use any of the client-side stuff
+        input = null!;
+        audio = null!;
+    }
+
     public void Reload() {
         try {
-            logger.Info($"PER v{version}");
-            logger.Info("Reloading game");
-
             if(resources.loaded) {
+                logger.Info("Reloading game");
                 resources.Unload();
                 game.Unload();
             }
+            else {
+                logger.Info($"PER v{version}");
+                if(_headless)
+                    logger.Info("RUNNING IN HEADLESS MODE");
+                logger.Info("Loading game");
+            }
+
             game.Load();
             resources.Load();
             RendererSettings rendererSettings = game.Loaded();
+
+            if(_headless) {
+                RunHeadless(rendererSettings);
+                return;
+            }
 
             if(renderer.open && renderer.Reset(rendererSettings))
                 input.Reset();
@@ -68,8 +95,22 @@ public class Engine {
         logger.Info("Starting soft reload");
         resources.SoftReload();
         RendererSettings rendererSettings = game.Loaded();
-        if(renderer.open && renderer.Reset(rendererSettings))
+        if(!_headless && renderer.open && renderer.Reset(rendererSettings))
             input.Reset();
+    }
+
+    private void RunHeadless(RendererSettings rendererSettings) {
+        logger.Info("Starting game");
+        _clock.Reset();
+        renderer.Setup(rendererSettings);
+        game.Setup();
+        logger.Info("Setup finished");
+        while(renderer.open) {
+            TimeSpan time = _clock.time;
+            TryTick(time);
+            frameTime.Update(time);
+        }
+        Finish();
     }
 
     private void Run(RendererSettings rendererSettings) {
@@ -119,10 +160,13 @@ public class Engine {
     private void Finish() {
         resources.Unload();
         game.Unload();
-        input.Finish();
-        renderer.Finish();
+        if(!_headless) {
+            input.Finish();
+            renderer.Finish();
+        }
         game.Finish();
-        audio.Finish();
+        if(!_headless)
+            audio.Finish();
 
         logger.Info("nooooooo *dies*");
         LogManager.Shutdown();
---
+++ PER/src/Headless/HeadlessRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PER.Abstractions.Rendering;
+using PER.Util;
+
+namespace PER.Headless;
+
+internal class HeadlessRenderer : IRenderer {
+    public bool open { get; private set; }
+    public void Setup(RendererSettings settings) => open = true;
+    public void Close() {
+        open = false;
+        closed?.Invoke(this, EventArgs.Empty);
+    }
+
+    // everything else is an invalid operation.
+
+    public string title {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public int width => throw new InvalidOperationException();
+    public int height => throw new InvalidOperationException();
+    public int framerate {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public bool fullscreen {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public IFont? font {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public string? icon {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public bool focused => throw new InvalidOperationException();
+    public event EventHandler? focusChanged;
+    public event EventHandler? closed;
+    public Color background {
+        get => throw new InvalidOperationException();
+        set => throw new InvalidOperationException();
+    }
+    public Dictionary<string, IEffect?> formattingEffects => throw new InvalidOperationException();
+    public void Finish() => throw new InvalidOperationException();
+    public bool Reset(RendererSettings settings) => throw new InvalidOperationException();
+    public void Clear() => throw new InvalidOperationException();
+    public void Update(TimeSpan time) => throw new InvalidOperationException();
+    public void Draw() => throw new InvalidOperationException();
+    public void DrawCharacter(Vector2Int position, RenderCharacter character,
+        RenderOptions options = RenderOptions.Default, IEffect? effect = null) => throw new InvalidOperationException();
+    public void DrawText(Vector2Int position, ReadOnlySpan<char> text, Func<char, Formatting> formatter,
+        HorizontalAlignment align = HorizontalAlignment.Left, int maxWidth = 0) =>
+        throw new InvalidOperationException();
+    public RenderCharacter GetCharacter(Vector2Int position) => throw new InvalidOperationException();
+    public void AddEffect(IEffect effect) => throw new InvalidOperationException();
+    public void AddEffect(Vector2Int position, IEffect? effect) => throw new InvalidOperationException();
+    public bool IsCharacterEmpty(Vector2Int position) => throw new InvalidOperationException();
+    public bool IsCharacterEmpty(RenderCharacter renderCharacter) => throw new InvalidOperationException();
+    public bool IsCharacterDrawable(char character, RenderStyle style) => throw new InvalidOperationException();
+}
